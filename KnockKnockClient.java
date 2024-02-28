8c5f6ca55776863a0f1fe52bc9100a9e6c35afed
aamnahmalik <aamnah@icloud.com>
2024-02-27 23:07:15 -0500

    testing

--- KnockKnockClient.java
+++ KnockKnockClient.java
@@ -48,10 +48,8 @@ public class KnockKnockClient {
     // }
 
     public static void main(String[] args) throws IOException {
-        
         if (args.length != 2) {
-            System.err.println(
-                "Usage: java EchoClient <host name> <port number>");
+            System.err.println("Usage: java EchoClient <host name> <port number>");
             System.exit(1);
         }
 
@@ -59,33 +57,28 @@ public class KnockKnockClient {
         int portNumber = Integer.parseInt(args[1]);
 
         try (
-            Socket kkSocket = new Socket(hostName, portNumber);
-            PrintWriter out = new PrintWriter(kkSocket.getOutputStream(), true);
-            BufferedReader in = new BufferedReader(
-                new InputStreamReader(kkSocket.getInputStream()));
-        ) {
-            BufferedReader stdIn =
-                new BufferedReader(new InputStreamReader(System.in));
+                Socket kkSocket = new Socket(hostName, portNumber);
+                PrintWriter out = new PrintWriter(kkSocket.getOutputStream(), true);
+                BufferedReader in = new BufferedReader(new InputStreamReader(kkSocket.getInputStream()))) {
             String fromServer;
-            String fromUser;
 
             while ((fromServer = in.readLine()) != null) {
                 System.out.println("Server: " + fromServer);
-                if (fromServer.equals("Bye."))
+                if (fromServer.equals("Bye.")) {
                     break;
-                
-                fromUser = String.valueOf(WordCount.wordCount(fromServer));
-                if (fromUser != null) {
-                    System.out.println("Client: " + fromUser);
-                    out.println(fromUser);
                 }
+
+                // Count words in the received file content
+                int wordCount = WordCount.wordCount(fromServer);
+
+                System.out.println("Client: Word count: " + wordCount);
+                out.println(String.valueOf(wordCount)); // Send word count to server
             }
         } catch (UnknownHostException e) {
             System.err.println("Don't know about host " + hostName);
             System.exit(1);
         } catch (IOException e) {
-            System.err.println("Couldn't get I/O for the connection to " +
-                hostName);
+            System.err.println("Couldn't get I/O for the connection to " + hostName);
             System.exit(1);
         }
     }
