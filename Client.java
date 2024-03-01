4c3404003dfbed787eadeca6c9a25f8b9ba784ae
mmmartinon <milena.silva@quinnipiac.edu>
2024-03-01 18:57:26 -0500

    Client/Server communication

--- Client.java
+++ Client.java
@@ -27,20 +27,76 @@ public class Client
         {
             StringBuilder fromServer = new StringBuilder();
             char[] buffer = new char[1024];
+            char[] temp = new char[1024];
             int bytesRead;
 
-            while ((bytesRead = in.read(buffer)) != -1) 
+            while (true) 
             {
+                String receivedString = in.readLine();
+
+                if (receivedString != null && receivedString.equals("****\n")) {
+                    break;
+                }
+
+                // Handle null case
+                if (receivedString != null) {
+                    wordCount = WordCount.wordCount(receivedString.trim());
+                    System.out.println("Client: Word count of received substring: " + wordCount);
+                    out.println(wordCount);
+                } else {
+                    System.err.println("Received null string from server");
+                }
+
+                // Calculate word count for the received substring
+                wordCount = WordCount.wordCount(receivedString);
+
+                // Print word count on client terminal
+                System.out.println("Client: Word count of received substring: " + wordCount);
+
+                // Send word count to server
+                out.println(wordCount);
+
+                //DR J EXPLANATION THAT YOU DID 
+                // bytesRead = in.read(buffer);
+                // if (bytesRead == -1) {
+                //     break;
+                // }
+
+                // String receivedString = new String(buffer, 0, bytesRead);
+                // if (!receivedString.equals("****")) {
+                //     fromServer.append(receivedString);
+                // } else {
+                //     break;
+                // }
                 // append the received data to the StringBuilder
-                fromServer.append(buffer, 0, bytesRead);
+                //in.read(temp);
+
+                // if (String.valueOf(temp) != "****")
+                // {
+                //     //append data to buffer
+                //     fromServer.append(temp, 0, )
+                // }
+                // fromServer.append(buffer, 0, bytesRead);
             }
+
+            //int totalBytes = 5;
+            
+            // while(bytesRead <= totalBytes)
+            // {
+
+            // }
+
 
             // print the server's request
             System.out.println("Server: What is the word count of " + fromServer + "?");
 
             // count words in the entire string
-            wordCount += WordCount.wordCount(fromServer.toString());
-            out.println(String.valueOf(wordCount));
+            wordCount = WordCount.wordCount(fromServer.toString());
+            //out.println(wordCount + "\n");
+            
+            System.out.println(socket.getPort());
+            out.println("client message");
+            out.flush();
 
             // print word count of the string on the client terminal
             System.out.println("Client: The word count of " + fromServer + " is " + wordCount + ".");
@@ -63,7 +119,7 @@ public class Client
             //     // System.exit(1);
             // }
 
-            out.println(String.valueOf(wordCount));
+            // out.println(String.valueOf(wordCount));
 
             System.exit(1);
         } 
--- Server.java
+++ Server.java
@@ -15,105 +15,159 @@ public class Server
         // socket.close();
         // System.out.println("Server IP address: " + ipAddress);
  
-        Socket[] clientSockets = new Socket[5];
+        Socket[] clientSockets = new Socket[2];
         ServerSocket serverSocket = new ServerSocket(portNumber); 
+        try {
 
-        // Wait for 5 clients
-        for (int i = 0; i < 2; i++)
-        {
-            System.out.println("Waiting for client connection...");
-            clientSockets[i] = serverSocket.accept();
-            System.out.println("Client connected at: " + clientSockets[i].getInetAddress() + ":" + clientSockets[i].getPort());
-            
-        }
-
-        BufferedReader[] clientStreams = new BufferedReader[5];
-
-        for (int j = 0; j < 2; j++) 
-        {
-            clientStreams[j] = new BufferedReader(new InputStreamReader(clientSockets[j].getInputStream()));
-        }
-
-        PrintWriter[] clientWriters = new PrintWriter[5];
-        for (int j = 0; j < 2; j++) 
-        {
-            clientWriters[j] = new PrintWriter(clientSockets[j].getOutputStream(), true);
-        }
-
-        try 
-        ( 
-            //initiate server socket on port
-            //connect to client
-            // Socket clientSocket = serverSocket.accept(); 
-
-            //create output/input streams
-            // PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true); 
-            FileInputStream fis = new FileInputStream(new File("words.txt"));
-            BufferedReader fileReader = new BufferedReader(new FileReader("words.txt"))
-        ) 
-        {
-            //send file path to client
-            //FindFile kkp = new FindFile();
-
-            byte[] fileBytes = fis.readAllBytes();
-            int numSubstrings = 2;
-            int approximateSubstringLength = fileBytes.length / numSubstrings;
-
-            String[] substrings = new String[numSubstrings];
-
-            for (int i = 0, start = 0; i < numSubstrings; i++) {
-                int end = start + approximateSubstringLength;
-            
-                // Find the last space character within the substring
-                while (end < fileBytes.length && fileBytes[end] != ' ') 
-                {
-                    end++;
-                }
-            
-                // Ensure end doesn't go out of bounds
-                end = Math.min(end, fileBytes.length);  // Clamp end to string length
-            
-                substrings[i] = new String(fileBytes, start, end - start);
-                clientWriters[i].println(substrings[i]);
-                System.out.println("hello");
-                start = end;
-            }
-            
-            // for (int i = 0, start = 0; i < numSubstrings; i++) {
-            //     int end = start + approximateSubstringLength;
-
-            //     // Find the last space character within the substring
-            //     while (end < fileBytes.length && fileBytes[end] != ' ') 
-            //     {
-            //         end++;
-            //     }
-
-            //     substrings[i] = new String(fileBytes, start, end - start);
-            //     clientWriters[i].println(substrings[i]);
-            //     start = end;
-            // }
-
-            // Read and print word counts from each client
-            for (int i = 0; i < 2; i++) 
+            // Wait for 5 clients
+            for (int i = 0; i < 2; i++)
             {
-                String count = clientStreams[i].readLine();
-                System.out.println("Word count from client " + i + ": " + count);
+                System.out.println("Waiting for client connection...");
+                clientSockets[i] = serverSocket.accept();
+                System.out.println("Client connected at: " + clientSockets[i].getInetAddress() + ":" + clientSockets[i].getPort());
+                
             }
 
+            BufferedReader[] clientStreams = new BufferedReader[2];
+            PrintWriter[] clientWriters = new PrintWriter[2];
+
+            for (int j = 0; j < 2; j++) 
+            {
+                clientStreams[j] = new BufferedReader(new InputStreamReader(clientSockets[j].getInputStream()));
+                clientWriters[j] = new PrintWriter(clientSockets[j].getOutputStream(), true);
+            }
+
+
+            try 
+            ( 
+                //initiate server socket on port
+                //connect to client
+                // Socket clientSocket = serverSocket.accept(); 
+
+                //create output/input streams
+                // PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true); 
+                FileInputStream fis = new FileInputStream(new File("words.txt"));
+                // BufferedReader fileReader = new BufferedReader(new FileReader("words.txt"))
+            ) 
+            {
+                //send file path to client
+                //FindFile kkp = new FindFile();
+
+                byte[] fileBytes = fis.readAllBytes();
+                int numClients = clientSockets.length;
+                int bytesPerClient = fileBytes.length / numClients;
+
+                // Handle unequal file sizes
+                int remainingBytes = fileBytes.length % numClients;
+
+
+                // for (int i = 0, start = 0; i < numClients; i++) {
+                //     int end = start + bytesPerClient;
+                
+                //     // Find the last space character within the substring
+                //     while (end < fileBytes.length && fileBytes[end] != ' ') 
+                //     {
+                //         end++;
+                //     }
+                
+                //     // Ensure end doesn't go out of bounds
+                //     end = Math.min(end, fileBytes.length);  // Clamp end to string length
+                
+                //     substrings[i] = new String(fileBytes, start, end - start);
+                //     clientWriters[i].println(substrings[i]);
+                //     System.out.println("hello");
+                //     start = end;
+                // }
+                
+                for (int i = 0; i < numClients; i++) {
+                    int clientBytes = bytesPerClient;
+                    if (i == numClients - 1) {
+                        clientBytes += remainingBytes; // Send remaining bytes to the last client
+                    }
+
+                    byte[] clientData = new byte[clientBytes];
+                    System.arraycopy(fileBytes, i * bytesPerClient, clientData, 0, clientBytes);
+
+                    clientWriters[i].println(new String(clientData));
+                    clientWriters[i].flush();
+                }
+
+                // for (int i = 0, start = 0; i < numSubstrings; i++) {
+                //     int end = start + approximateSubstringLength;
+
+                //     // Find the last space character within the substring
+                //     while (end < fileBytes.length && fileBytes[end] != ' ') 
+                //     {
+                //         end++;
+                //     }
+
+                //     end = Math.min(end, fileBytes.length);
+
+                //     substrings[i] = new String(fileBytes, start, end - start);
+                //     clientWriters[i].println(substrings[i]);
+                //     clientWriters[i].flush();
+                //     clientWriters[i].println("****");
+                //     clientWriters[i].flush();
+
+                //     start = end;
+                // }
+
+                // Read and print word counts from each client
+                System.out.println("before for");
+
+                 // Read word counts from clients
+                 int[] clientWordCounts = new int[numClients];
+                 for (int i = 0; i < numClients; i++) {
+                     String count = clientStreams[i].readLine();
+                     clientWordCounts[i] = Integer.parseInt(count);
+                     System.out.println("Word count from client " + i + ": " + count);
+                 }
+ 
+                 // Calculate total word count
+                 int totalWordCount = 0;
+                 for (int count : clientWordCounts) {
+                     totalWordCount += count;
+                 }
+
+                // System.out.println(clientStreams[0].readLine());
+
+                // for (int i = 0; i < 2; i++) 
+                // {
+                //     String count = clientStreams[i].readLine();
+                //     System.out.println("Word count from client " + i + ": " + count);
+                // }
+
+                System.out.println("Total word count: " + totalWordCount);
+
+            } 
+            catch (IOException e) 
+            {
+                System.out.println("Exception caught when trying to listen on port " + portNumber + " or listening for a connection");
+                System.out.println(e.getMessage());
+            }
+        }
+        catch (IOException e) {
+            System.err.println("Server error: " + e.getMessage());
         } 
-        catch (IOException e) 
+        finally 
         {
-            System.out.println("Exception caught when trying to listen on port " + portNumber + " or listening for a connection");
-            System.out.println(e.getMessage());
-        }
-    }
-
-    private static int findSpaceCharacter(byte[] bytes, int endIndex) {
-        for (int i = endIndex; i >= 0; i--) {
-            if (bytes[i] == ' ') {
-                return i;
+            if (serverSocket != null) {
+                serverSocket.close();
             }
+            for (Socket socket : clientSockets) {
+                if (socket != null) {
+                    socket.close();
+                }
+            }
+
         }
-        return endIndex; // If no space character is found, return the original endIndex
     }
+    // private static int findSpaceCharacter(byte[] bytes, int endIndex) {
+    //     for (int i = endIndex; i >= 0; i--) {
+    //         if (bytes[i] == ' ') {
+    //             return i;
+    //         }
+    //     }
+    //     return endIndex; // If no space character is found, return the original endIndex
+    // }
 }
